c98d438dbd17ea679ea9110408a5a556afb62d9e
Yaroslav-662 <paroslav662@gmail.com>
2025-12-05 22:55:42 +0200

    Update level2-jwt-auth-test.js

--- tests/level2-jwt-auth-test.js
+++ tests/level2-jwt-auth-test.js
@@ -1,13 +1,11 @@
 import { io } from "socket.io-client";
 
-const token = "PASTE_YOUR_JWT_HERE";
+const URL = process.env.TEST_URL || "https://ecommerce-backend-laba3.onrender.com";
+const token = process.env.TEST_JWT || "PASTE_YOUR_JWT_HERE";
 
-const socket = io("http://localhost:5000", {
-  auth: { token }
-});
+const socket = io(URL, { auth: { token } });
 
 socket.on("connect", () => console.log("Authorized:", socket.id));
-
 socket.on("auth_error", (err) => console.log("AUTH ERROR:", err));
 
 setTimeout(() => process.exit(0), 3000);

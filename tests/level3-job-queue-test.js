87f368be0c5346ac40a1ebc8fb2c78223d40b6b1
Yaroslav-662 <paroslav662@gmail.com>
2025-12-06 01:37:02 +0200

    Update level3-job-queue-test.js

--- tests/level3-job-queue-test.js
+++ tests/level3-job-queue-test.js
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
-const URL = process.env.TEST_URL || "https://ecommerce-backend-laba3.onrender.com";
-const socket = io(URL);
+const URL = process.env.TEST_URL;
+const token = process.env.TEST_JWT;
+
+const socket = io(URL, { auth: { token } });
 
 socket.on("job_done", (msg) => {
   console.log("JOB PROCESSED:", msg);

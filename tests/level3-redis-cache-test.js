61d1285faced1ea51fb0abf9848c06d8d0fcba40
Yaroslav-662 <paroslav662@gmail.com>
2025-12-05 23:02:24 +0200

    Update level3-redis-cache-test.js

--- tests/level3-redis-cache-test.js
+++ tests/level3-redis-cache-test.js
@@ -1,12 +1,14 @@
 import axios from "axios";
 
+const URL = process.env.TEST_URL || "https://ecommerce-backend-laba3.onrender.com";
+
 (async () => {
   console.time("no-cache");
-  await axios.get("http://localhost:5000/api/products?nocache=1");
+  await axios.get(`${URL}/api/products?nocache=1`);
   console.timeEnd("no-cache");
 
   console.time("cache");
-  await axios.get("http://localhost:5000/api/products");
+  await axios.get(`${URL}/api/products`);
   console.timeEnd("cache");
 
   process.exit(0);

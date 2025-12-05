06b509160b53ffbd7b4af31e098712e5fb20ea25
Yaroslav-662 <paroslav662@gmail.com>
2025-12-06 01:35:47 +0200

    Update level2-reconnection-test.js

--- tests/level2-reconnection-test.js
+++ tests/level2-reconnection-test.js
@@ -1,7 +1,9 @@
 import { io } from "socket.io-client";
 
-const URL = process.env.TEST_URL || "https://ecommerce-backend-laba3.onrender.com";
-const socket = io(URL, { reconnectionAttempts: 5, reconnectionDelay: 500 });
+const URL = process.env.TEST_URL;
+const token = process.env.TEST_JWT;
+
+const socket = io(URL, { auth: { token }, reconnectionAttempts: 5, reconnectionDelay: 500 });
 
 socket.on("connect", () => console.log("Connected:", socket.id));
 socket.on("reconnect_attempt", () => console.log("Trying reconnect..."));

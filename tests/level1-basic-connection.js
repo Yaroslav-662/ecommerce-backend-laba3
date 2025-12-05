3c946dc36d0e46237ff908e388841d9f6cf3217c
Yaroslav-662 <paroslav662@gmail.com>
2025-12-05 22:44:34 +0200

    Update level1-basic-connection.js

--- tests/level1-basic-connection.js
+++ tests/level1-basic-connection.js
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const URL = process.env.TEST_URL || "http://localhost:5000";
+
+const socket = io(URL, { transports: ["websocket"] });
 
 socket.on("connect", () => {
   console.log("Connected:", socket.id);

f8947fdee96990bce5df10ce9415394bab2c1001
Yaroslav-662 <paroslav662@gmail.com>
2025-12-06 01:37:43 +0200

    Update level3-metrics-test.js

--- tests/level3-metrics-test.js
+++ tests/level3-metrics-test.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const URL = process.env.TEST_URL || "https://ecommerce-backend-laba3.onrender.com";
+const URL = process.env.TEST_URL;
+const token = process.env.TEST_JWT;
 
 (async () => {
-  const metrics = await axios.get(`${URL}/metrics`);
+  const metrics = await axios.get(`${URL}/metrics`, { headers: { Authorization: token } });
   console.log(metrics.data);
   process.exit(0);
 })();

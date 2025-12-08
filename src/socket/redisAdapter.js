470eb78d7eaf3f3832a1063f5728207338a5f0c1
Yaroslav-662 <paroslav662@gmail.com>
2025-12-08 21:37:37 +0200

    Update redisAdapter.js

--- src/socket/redisAdapter.js
+++ src/socket/redisAdapter.js
@@ -1,21 +1,22 @@
 // src/socket/redisAdapter.js
-import { createAdapter } from "@socket.io/redis-adapter";
-import { createClient } from "redis";
+//import { createAdapter } from "@socket.io/redis-adapter";
+//import { createClient } from "redis";
 
 /**
  * Expects process.env.REDIS_URL
  */
-export default async function initRedisAdapter(io) {
-  const url = process.env.REDIS_URL;
-  if (!url) throw new Error("REDIS_URL not provided");
+//export default async function initRedisAdapter(io) {
+ // const url = process.env.REDIS_URL;
+ // if (!url) throw new Error("REDIS_URL not provided");
 
-  const pubClient = createClient({ url });
-  const subClient = pubClient.duplicate();
+  //const pubClient = createClient({ url });
+  //const subClient = pubClient.duplicate();
 
-  await pubClient.connect();
-  await subClient.connect();
+ // await pubClient.connect();
+ // await subClient.connect();
 
-  io.adapter(createAdapter(pubClient, subClient));
-  console.log("✅ Socket.IO Redis adapter initialized");
-  return { pubClient, subClient };
+ // io.adapter(createAdapter(pubClient, subClient));
+ // console.log("✅ Socket.IO Redis adapter initialized");
+ // return { pubClient, subClient };
 }
+
